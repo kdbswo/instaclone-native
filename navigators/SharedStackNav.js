21df500cdcd38af13b73e64efc20eb579212a5f1
kdbswo <kdbswo@gmail.com>
2023-01-20 08:17:29 +0900

    Photo #2
    image size , getSize, icons,

--- navigators/SharedStackNav.js
+++ navigators/SharedStackNav.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Image } from "react-native";
+import Comments from "../screens/Comments";
 import Feed from "../screens/Feed";
+import Likes from "../screens/Likes";
 import Me from "../screens/Me";
 import Notifications from "../screens/Notifications";
 import Photo from "../screens/Photo";
@@ -49,6 +51,8 @@ export default function SharedStackNav({ screenName }) {
       {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Photo" component={Photo} />
+      <Stack.Screen name="Likes" component={Likes} />
+      <Stack.Screen name="Comments" component={Comments} />
     </Stack.Navigator>
   );
 }
