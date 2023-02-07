f6db141b8fdce4fb7f3b29e196337e0d78463f53
kdbswo <kdbswo@gmail.com>
2023-02-07 11:28:15 +0900

    Take Photo #1
    set zoom slider, camera type(front, back) in the camera screen

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,9 +1,89 @@
-import { Text, View } from "react-native";
+import { Camera } from "expo-camera";
+import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import Slider from "@react-native-community/slider";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Actions = styled.View`
+  flex: 0.35;
+  padding: 0px 50px;
+  align-items: center;
+  justify-content: space-around;
+`;
+const ButtonsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TakePhotoBtn = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 50px;
+  border: 1px solid rgba(255, 255, 255, 0.8);
+`;
+
+const SliderContainer = styled.View``;
 
 export default function TakePhoto() {
+  const [ok, setOk] = useState(false);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+  const [zoom, setZoom] = useState(0);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const getPermissions = async () => {
+    const { granted } = await Camera.requestCameraPermissionsAsync();
+    setOk(granted);
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  const onCameraSwitch = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+  const onZoomValueChange = (e) => {
+    setZoom(e);
+  };
   return (
-    <View>
-      <Text>TakePhoto</Text>
-    </View>
+    <Container>
+      <Camera type={cameraType} style={{ flex: 1 }} zoom={zoom} />
+      <Actions>
+        <SliderContainer>
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={0}
+            maximumValue={1}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="rgba(255,255,255,0.5)"
+            onValueChange={onZoomValueChange}
+          />
+        </SliderContainer>
+        <ButtonsContainer>
+          <TakePhotoBtn />
+          <TouchableOpacity onPress={onCameraSwitch}>
+            <Ionicons
+              color="white"
+              size={30}
+              name={
+                cameraType === Camera.Constants.Type.front
+                  ? "camera-reverse"
+                  : "camera"
+              }
+            />
+          </TouchableOpacity>
+        </ButtonsContainer>
+      </Actions>
+    </Container>
   );
 }
